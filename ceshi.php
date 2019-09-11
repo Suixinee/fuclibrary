804ce24290be1bdfad0ff6e45b23cf6a8afac300
Suixinee <1172141559@qq.com>
2019-09-11 19:02:05 +0800

    alg commit

--- ceshi.php
+++ ceshi.php
@@ -27,4 +27,24 @@ $data = [
 echo "<pre>";
 var_dump(array_sort($data,'id','asc'));
 
-var_dump(arr_multi2single($data));
+var_dump(array_multi2single($data));
+
+function insert_srot($arr){
+    if(empty($arr)&&is_array($arr)){
+        $len = count($arr);
+        for($i=1;$i<$len;$i++){
+            $tmp = $arr[$i];
+            for($j=$i-1;$j>=0;$j++){
+                if($arr[$j]>$tmp){
+                    $arr[$j+1]=$arr[$j];
+                    $arr[$j] = $tmp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    return $arr;
+}
+
+var_dump(insert_srot([7,5,4,11,2]));
